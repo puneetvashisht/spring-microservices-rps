28d6b737e1aedef58e7ca6489b6a11bb7ffd67b8
puneetvashisht <puneetvashisht@gmail.com>
2024-03-12 16:40:24 +0530

    final spring data jpa

--- spring-boot/workout-tracker/src/main/java/com/rps/workouttracker/controllers/WorkoutController.java
+++ spring-boot/workout-tracker/src/main/java/com/rps/workouttracker/controllers/WorkoutController.java
@@ -28,6 +28,43 @@ public class WorkoutController {
         return workoutRepository.findAll();
     }
 
+    @GetMapping("/workouts/title/{title}")
+    public List<Workout> getWorkoutsByTitle(@PathVariable String title) {
+        // return workouts by title or throw exception if not found
+        List<Workout> workouts = workoutRepository.findByTitle(title);
+        if(workouts.isEmpty()){
+            throw new ResourceNotFoundException("Workout not found");
+        }
+        else{
+            return workouts;
+        }
+    }
+
+    @GetMapping("/workouts/cbpm/{cbpm}")
+    public List<Workout> getWorkoutsByCbpm(@PathVariable int cbpm) {
+        // return workouts by cbpm or throw exception if not found
+        List<Workout> workouts = workoutRepository.findWorkoutGreaterThanCbpm(cbpm);
+        if(workouts.isEmpty()){
+            throw new ResourceNotFoundException("Workout not found");
+        }
+        else{
+            return workouts;
+        }
+    }
+
+    @GetMapping("/workouts/category/{id}")
+    public List<Workout> getWorkoutsByCategory(@PathVariable int id) {
+        // return workouts by category id or throw exception if not found
+        Optional<Category> categoryFound = categoryRepository.findById(id);
+        if(categoryFound.isEmpty()){
+            throw new ResourceNotFoundException("Category not found");
+        }
+        else{
+            return workoutRepository.findByCategory(categoryFound.get());
+        }
+    }
+
+
     @GetMapping("/workouts/{id}")
     public Workout getWorkout(@PathVariable int id) {
         // return workout by id or throw exception if not found
--- spring-boot/workout-tracker/src/main/java/com/rps/workouttracker/repositories/WorkoutRepository.java
+++ spring-boot/workout-tracker/src/main/java/com/rps/workouttracker/repositories/WorkoutRepository.java
@@ -1,9 +1,20 @@
 package com.rps.workouttracker.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.rps.workouttracker.entities.Category;
 import com.rps.workouttracker.entities.Workout;
 
 public interface WorkoutRepository extends JpaRepository<Workout, Integer>{
+
+
+    @Query("SELECT w FROM Workout w WHERE w.cbpm > :cbpm")
+    List<Workout> findWorkoutGreaterThanCbpm(Integer cbpm);
+
+    List<Workout> findByCategory(Category category);
+    List<Workout> findByTitle(String title);
     
 }
