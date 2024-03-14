c651e21742f9fd9c5c9b6dafa18df9a8aac2a8a6
puneetvashisht <puneetvashisht@gmail.com>
2024-03-14 14:51:33 +0530

    falut tolerance

--- spring-boot/workout-active-service/src/main/java/com/rps/workoutactiveservice/services/WorkoutActiveService.java
+++ spring-boot/workout-active-service/src/main/java/com/rps/workoutactiveservice/services/WorkoutActiveService.java
@@ -1,19 +1,32 @@
 package com.rps.workoutactiveservice.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.rps.workoutactiveservice.controllers.WorkoutServiceProxy;
 import com.rps.workoutactiveservice.dto.Workout;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 
 @Service
 public class WorkoutActiveService {
 	@Autowired
     WorkoutServiceProxy workoutServiceProxy;
+
+	@Autowired
+	CircuitBreakerFactory circuitBreakerFactory;
+
+	private CircuitBreaker circuitBreaker = CircuitBreaker.of("workoutService", 
+			CircuitBreakerConfig.custom()
+			.failureRateThreshold(50)
+			.waitDurationInOpenState(java.time.Duration.ofMillis(1000))
+			.slidingWindowSize(2)
+			.build());
+
 	
-	@HystrixCommand(fallbackMethod = "defaultWorkout")
+	@io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker(name = "workoutService", fallbackMethod = "defaultWorkout")
 	public Workout fetchWorkout(int workoutId) throws Exception {
 		try {
 			Workout workout = workoutServiceProxy.fetchWorkout(workoutId);
@@ -26,10 +39,10 @@ public class WorkoutActiveService {
 			System.out.println("Exception ***********");
 			throw new RuntimeException("workout not found!");
 		}
-		
 	}
 	
-	public Object defaultWorkout() {
+	public Workout defaultWorkout(int workoutId, Exception e) {
+		System.out.println("Fallback method called***********	");
         return new Workout(1, "Running", "Good for health");
     }
 }
